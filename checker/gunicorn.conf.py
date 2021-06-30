557ec321fdd8869e8a60d29da3a7f8ba698e94f7
ldruschk <14059613+ldruschk@users.noreply.github.com>
2021-06-30 12:11:40 +0200

    migrate to enochecker3 (#3)
    * migrate to enochecker3
    
    * fix lints
    
    * backwards compatibility with py3.7

--- checker/gunicorn.conf.py
+++ checker/gunicorn.conf.py
@@ -1,10 +1,6 @@
 import multiprocessing
 
-worker_class = "gevent"
-workers = multiprocessing.cpu_count()*2+1
+worker_class = "uvicorn.workers.UvicornWorker"
+workers = min(4, multiprocessing.cpu_count())
 bind = "0.0.0.0:8000"
 timeout = 90
-keepalive = 3600
-max_requests = 100
-preload_app = True
-max_requests_jitter = 30
