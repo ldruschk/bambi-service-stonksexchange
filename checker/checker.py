949775c9453c4994d7d2ed1ae0a537f0bd6900af
Lucas Druschke <ldruschk@posteo.de>
2021-07-07 12:15:40 +0200

    improve exception handling in checker

--- checker/checker.py
+++ checker/checker.py
@@ -4,7 +4,7 @@ from typing import Optional, Tuple
 
 from enochecker3 import ChainDB, Enochecker, GetflagCheckerTaskMessage, MumbleException, PutflagCheckerTaskMessage
 from enochecker3.utils import FlagSearcher, assert_equals, assert_in
-from httpx import AsyncClient
+from httpx import AsyncClient, RequestError
 
 checker = Enochecker("StonksExchange", 8199)
 app = lambda: checker.app
@@ -13,30 +13,42 @@ app = lambda: checker.app
 async def register_user(client: AsyncClient) -> Tuple[str, str]:
     username = secrets.token_hex(6)
     password = secrets.token_hex(8)
-    response = await client.post("/register", data={"username": username, "password": password,},)
+    try:
+        response = await client.post("/register", data={"username": username, "password": password})
+    except RequestError:
+        raise MumbleException("request error while logging in")
     assert_equals(response.status_code, 200, "registration failed")
 
     return username, password
 
 
 async def login_user(client: AsyncClient, username: str, password: str) -> None:
-    response = await client.post("/login", data={"username": username, "password": password,},)
+    try:
+        response = await client.post("/login", data={"username": username, "password": password})
+    except RequestError:
+        raise MumbleException("request error while logging in")
     assert_equals(response.status_code, 200, "login failed")
 
 
 async def send_message(client: AsyncClient, username: str, message: str) -> None:
-    response = await client.post("/message", data={"username": username, "message": message,},)
+    try:
+        response = await client.post("/message", data={"username": username, "message": message})
+    except RequestError:
+        raise MumbleException("request error while sending message")
     assert_equals(response.status_code, 200, "sending message failed")
 
 
 async def receive_message(client: AsyncClient, message: str) -> None:
-    response = await client.get("/messages")
+    try:
+        response = await client.get("/messages")
+    except RequestError:
+        raise MumbleException("request error while receiving message")
     assert_equals(response.status_code, 200, "receiving message failed")
     assert_in(message, response.text, "flag missing from retrieved messages")
 
 
 @checker.putflag(0)
-async def putflag_test(task: PutflagCheckerTaskMessage, session_a: AsyncClient, session_b: AsyncClient, db: ChainDB, logger: LoggerAdapter,) -> None:
+async def putflag_test(task: PutflagCheckerTaskMessage, session_a: AsyncClient, session_b: AsyncClient, db: ChainDB, logger: LoggerAdapter) -> None:
     await register_user(session_a)
     logger.debug("registered user for session_a")
     (username_b, password_b) = await register_user(session_b)
@@ -63,10 +75,10 @@ async def exploit_test(searcher: FlagSearcher, client: AsyncClient) -> Optional[
     username = "0" * 12 + secrets.token_hex(6)
     password = secrets.token_hex(8)
 
-    r = await client.post("/register", data={"username": username, "password": password,},)
+    r = await client.post("/register", data={"username": username, "password": password})
     assert not r.is_error
 
-    r = await client.post("/login", json={"username": {"$gte": username,}, "password": password,},)
+    r = await client.post("/login", json={"username": {"$gte": username}, "password": password})
     assert not r.is_error
 
     r = await client.get("/messages")
